ad0fc3d0fef7212812ec4150b07543fd4d0f5b91
DevanshuDesai15 <devanshudesai_1509@yahoo.com>
2023-06-17 13:57:33 -0700

    code: M-problem3

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -6,8 +6,37 @@
   - `npm run test-palindrome`
 */
 
+function reverse(str) {
+  let answer = "";
+  for(let i = str.length -1; i>=0; i--) {
+    answer += str[i];
+  }
+  return answer;
+}
+
+function transform(str) {
+  let answer = "";
+  for (let i = 0; i<str.length; i++) {
+    if (str[i] === " " || str[i] === "," || str[i] === "?" || str[i] === "!" || str[i] === ".") {
+
+    }else{
+      answer += str[i];
+    }
+  }
+  return answer;
+}
+
 function isPalindrome(str) {
-  return true;
+  str = str.toLowerCase();
+  str = transform(str);
+
+  let reverseStr = reverse(str);
+  if (reverseStr === str) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
 
 module.exports = isPalindrome;
